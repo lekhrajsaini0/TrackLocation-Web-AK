930c5b471e19c7b9fb02994f1da472c897c974e6
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-03-21 19:11:51 +0530

    Device Info Card is added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { Map, View } from "ol";
+import { Map, Overlay, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { fromLonLat, transform } from "ol/proj.js";
 import OSM from "ol/source/OSM";
@@ -12,6 +12,7 @@ import Icon from "ol/style/Icon";
 
 import { db } from "./config";
 import { ref, onValue } from "firebase/database";
+import Stroke from "ol/style/Stroke";
 
 let longitude;
 let latitude;
@@ -23,7 +24,6 @@ onValue(starCountRef, (snapshot) => {
   // console.log(data.user1.lon);
   longitude = data.user1.lon;
   latitude = data.user1.lat;
- 
 
   const map = new Map({
     target: "map",
@@ -61,11 +61,36 @@ onValue(starCountRef, (snapshot) => {
 
   map.addLayer(markerLayer);
 
-  // window.setInterval(() => {
-  //   markerFeature.getGeometry().setCoordinates(fromLonLat([longitude, latitude]));
-  //   console.log(longitude)
-  //   console.log(latitude)
-  // }, 5000);
-  
- 
+  //showing user IP with marker
+  var overlay = new Overlay({
+    position: fromLonLat([longitude, latitude]),
+    positioning: 'bottom-center',
+    element: document.createElement("div"),
+    stopEvent: false,
+  });
+
+  overlay.getElement().innerHTML = `<div class="card">
+  <div class="card-header">
+    Device Info
+  </div>
+  <div class="card-body">
+    <p><strong>Device Name:</strong> ${data.user1.brand}</p>
+    <p><strong>IP Address:</strong> ${data.user1.ip}</p>
+    <p><strong>Longitude:</strong> ${data.user1.lat}</p>
+    <p><strong>Latitude:</strong> ${data.user1.lon}</p>
+  </div>
+</div>`
+
+  overlay.getElement().style.fontSize = "25px";
+  map.addOverlay(overlay);
+
+  // Code to update the marker in every 5 sec.
+  window.setInterval(() => {
+    markerFeature
+      .getGeometry()
+      .setCoordinates(fromLonLat([longitude, latitude]));
+    // overlay.setPosition([longitude, latitude]);
+    console.log(longitude);
+    console.log(latitude);
+  }, 2000);
 });
